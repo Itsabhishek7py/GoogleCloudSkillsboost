93d4e7e28e8064eae72c0691ef9345e29bcaf060
nov05 <1215590+nov05@users.noreply.github.com>
2026-05-09 13:04:35 -0500

    .
    Refactor gcloud command for creating GKE cluster into a multi-line format for better readability.

--- Implement Cloud Security Fundamentals on Google Cloud: Challenge Lab/drabhishek.sh
+++ Implement Cloud Security Fundamentals on Google Cloud: Challenge Lab/drabhishek.sh
@@ -108,7 +108,18 @@ gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
 
 echo "🏗️ Creating GKE cluster..."
 echo "   This may take a few minutes..."
-gcloud container clusters create $CLUSTER_NAME --num-nodes 1 --master-ipv4-cidr=172.16.0.64/28 --network orca-build-vpc --subnetwork orca-build-subnet --enable-master-authorized-networks  --master-authorized-networks 192.168.10.2/32 --enable-ip-alias --enable-private-nodes --enable-private-endpoint --service-account $SERVICE_ACCOUNT@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --zone $ZONE
+gcloud container clusters create $CLUSTER_NAME \
+    --num-nodes 1 \
+    --master-ipv4-cidr=172.16.0.64/28 \
+    --network orca-build-vpc \
+    --subnetwork orca-build-subnet \
+    --enable-master-authorized-networks  \
+    --master-authorized-networks 192.168.10.2/32 \
+    --enable-ip-alias \
+    --enable-private-nodes \
+    --enable-private-endpoint \
+    --service-account $SERVICE_ACCOUNT@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com \
+    --zone $ZONE
 
 # Task 5:-
 echo "🔗 Configuring jumphost and deploying application..."
