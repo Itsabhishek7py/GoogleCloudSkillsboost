624988e5893307186b6978f1fc3165679c4b3e4a
nov05 <1215590+nov05@users.noreply.github.com>
2026-05-09 16:58:17 -0500

    .

--- Google Kubernetes Engine Pipeline using Cloud Build/abhishek.sh
+++ Google Kubernetes Engine Pipeline using Cloud Build/abhishek.sh
@@ -30,7 +30,7 @@ spinner() {
     local i=0
     while kill -0 "$pid" 2>/dev/null; do
         i=$(( (i+1) % 4 ))
-        printf "\rLoading... [%c]   " "${spin:$i:1}"
+        printf "\r${CYAN}Loading...${NC} [%c]   " "${spin:$i:1}"
         sleep 0.1
     done
     printf "\r${GREEN}Done!         ${NC}\n\n"  
