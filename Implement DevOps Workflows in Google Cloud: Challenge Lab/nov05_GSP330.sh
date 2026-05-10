a76a3337a1fa2a02657f3a4f23294707da386c87
nov05 <1215590+nov05@users.noreply.github.com>
2026-05-09 20:14:27 -0500

    .

--- Implement DevOps Workflows in Google Cloud: Challenge Lab/nov05_GSP330.sh
+++ Implement DevOps Workflows in Google Cloud: Challenge Lab/nov05_GSP330.sh
@@ -19,7 +19,7 @@ spinner() {
     local i=0
     while kill -0 "$pid" 2>/dev/null; do
         i=$(( (i+1) % 4 ))
-        printf "\r${CYAN}Loading...${RESET_FORMAT} [%c]   " "${spin:$i:1}"
+        printf "\r${CYAN_TEXT}Loading...${RESET_FORMAT} [%c]   " "${spin:$i:1}"
         sleep 0.1
     done
     printf "\r${GREEN_TEXT}Done!         ${RESET_FORMAT}\n\n"  
