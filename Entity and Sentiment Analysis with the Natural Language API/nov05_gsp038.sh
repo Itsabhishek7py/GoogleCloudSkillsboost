08e3889a59097f8fdfb5911e323ff680ed9ed440
nov05 <1215590+nov05@users.noreply.github.com>
2026-05-11 15:42:20 -0500

    .

--- Entity and Sentiment Analysis with the Natural Language API/nov05_gsp038.sh
+++ Entity and Sentiment Analysis with the Natural Language API/nov05_gsp038.sh
@@ -72,14 +72,12 @@ cat request.json
 ## Task 3. Call the Natural Language API
 ###################################################################
 
-curl "https://language.googleapis.com/v1/documents:analyzeEntities?key=${API_KEY}" \
-  -s -X POST -H "Content-Type: application/json" --data-binary @request.json > result.json
-
 echo -e "\n👉  Analysis result:"
-cat result.json
+curl "https://language.googleapis.com/v1/documents:analyzeEntities?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request.json
 
 ## Clean up 
-rm -f task.sh request.json result.json
+rm -f task.sh request.json
 EOF
 
 # Copy the script into $HOME dir
@@ -102,7 +100,7 @@ EOF
 
 rm -f request.json
 cat > request.json <<'EOF'
- {
+{
   "document":{
     "type":"PLAIN_TEXT",
     "content":"Harry Potter is the best book. I think everyone should read it."
@@ -114,14 +112,13 @@ EOF
 rm -f task.sh
 cat > task.sh <<'EOF'
 #!/bin/bash
-export API_KEY=$(cat ~/api_key.txt)
+export API_KEY=$(< ~/api_key.txt)
 echo -e "/n👉  Analysis request:"
 cat request.json
-curl "https://language.googleapis.com/v1/documents:analyzeEntities?key=${API_KEY}" \
-  -s -X POST -H "Content-Type: application/json" --data-binary @request.json > result.json
 echo -e "👉  Analysis result:"
-cat result.json
-rm -f task.sh request.json result.json
+curl "https://language.googleapis.com/v1/documents:analyzeSentiment?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request.json
+rm -f task.sh request.json
 EOF
 
 gcloud compute scp task.sh request.json linux-instance:~ \
@@ -140,14 +137,115 @@ cat << 'EOF'
 ###################################################################
 EOF
 
+rm -f request.json
+cat > request.json <<'EOF'
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content":"I liked the sushi but the service was terrible."
+  },
+  "encodingType": "UTF8"
+}
+EOF
+
+rm -f task.sh
+cat > task.sh <<'EOF'
+#!/bin/bash
+export API_KEY=$(< ~/api_key.txt)
+echo -e "/n👉  Analysis request:"
+cat request.json
+echo -e "👉  Analysis result:"
+curl "https://language.googleapis.com/v1/documents:analyzeEntitySentiment?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request.json
+cat result.json
+rm -f task.sh request.json
+EOF
+
+gcloud compute scp task.sh request.json linux-instance:~ \
+  --project=$DEVSHELL_PROJECT_ID \
+  --zone=$ZONE \
+  --quiet
+gcloud compute ssh linux-instance \
+  --project=$DEVSHELL_PROJECT_ID \
+  --zone=$ZONE \
+  --quiet \
+  --command="chmod +x task.sh && task.sh"
+  
 cat << 'EOF'
 ###################################################################
 ## Task 6. Analyzing syntax and parts of speech
 ###################################################################
 EOF
 
+rm -f request.json
+cat > request.json <<'EOF'
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content": "Joanne Rowling is a British novelist, screenwriter and film producer."
+  },
+  "encodingType": "UTF8"
+}
+EOF
+
+rm -f task.sh
+cat > task.sh <<'EOF'
+#!/bin/bash
+export API_KEY=$(< ~/api_key.txt)
+echo -e "/n👉  Analysis request:"
+cat request.json
+echo -e "👉  Analysis result:"
+curl "https://language.googleapis.com/v1/documents:analyzeSyntax?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request.json
+cat result.json
+rm -f task.sh request.json
+EOF
+
+gcloud compute scp task.sh request.json linux-instance:~ \
+  --project=$DEVSHELL_PROJECT_ID \
+  --zone=$ZONE \
+  --quiet
+gcloud compute ssh linux-instance \
+  --project=$DEVSHELL_PROJECT_ID \
+  --zone=$ZONE \
+  --quiet \
+  --command="chmod +x task.sh && task.sh"
+  
 cat << 'EOF'
 ###################################################################
 ## Task 7. Multilingual natural language processing
 ###################################################################
 EOF
+
+rm -f request.json
+cat > request.json <<'EOF'
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content":"日本のグーグルのオフィスは、東京の六本木ヒルズにあります"
+  }
+}
+EOF
+
+rm -f task.sh
+cat > task.sh <<'EOF'
+#!/bin/bash
+export API_KEY=$(< ~/api_key.txt)
+echo -e "/n👉  Analysis request:"
+cat request.json
+echo -e "👉  Analysis result:"
+curl "https://language.googleapis.com/v1/documents:analyzeEntities?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @request.json
+cat result.json
+rm -f task.sh request.json
+EOF
+
+gcloud compute scp task.sh request.json linux-instance:~ \
+  --project=$DEVSHELL_PROJECT_ID \
+  --zone=$ZONE \
+  --quiet
+gcloud compute ssh linux-instance \
+  --project=$DEVSHELL_PROJECT_ID \
+  --zone=$ZONE \
+  --quiet \
+  --command="chmod +x task.sh && task.sh"
