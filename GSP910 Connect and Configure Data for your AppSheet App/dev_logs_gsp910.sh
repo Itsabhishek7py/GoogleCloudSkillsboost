7daf0b701ece710dd313ab5e704a903d2bd20628
nov05 <1215590+nov05@users.noreply.github.com>
2026-05-11 04:43:40 -0500

    Create dev_logs_gsp910.sh
    Added a development script and logging information.

---
+++ GSP910 Connect and Configure Data for your AppSheet App/dev_logs_gsp910.sh
@@ -0,0 +1,12 @@
+## 👉 For development
+
+```bash
+rm -f nov05_gsp910.sh
+curl -LO https://raw.githubusercontent.com/nov05/gcp-skills-boost/refs/heads/dev/GSP910%20Connect%20and%20Configure%20Data%20for%20your%20AppSheet%20App/nov05_gsp910.sh
+chmod +x nov05_gsp910.sh
+./nov05_gsp910.sh
+```
+
+## 👉 Logs
+
+* 2026-05-11 Created script `nov05_gsp910.sh`    
