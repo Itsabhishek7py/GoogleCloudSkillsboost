34c50bcc90156a89711279ceb98bb3207706b4c4
nov05 <1215590+nov05@users.noreply.github.com>
2026-05-11 12:38:42 -0500

    .

--- Cloud Natural Language API: Qwik Start/nov05_gsp097.sh
+++ Cloud Natural Language API: Qwik Start/nov05_gsp097.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 ## Created by nov05, 2026-05-11  
 
-# Bright Foreground Colors
-BLACK_TEXT=$'\033[0;90m'
-RED_TEXT=$'\033[0;91m'
-GREEN_TEXT=$'\033[0;92m'
-YELLOW_TEXT=$'\033[0;93m'
-BLUE_TEXT=$'\033[0;94m'
-MAGENTA_TEXT=$'\033[0;95m'
-CYAN_TEXT=$'\033[0;96m'
-WHITE_TEXT=$'\033[0;97m'
-## Text format
-NO_COLOR=$'\033[0m'
-RESET_FORMAT=$'\033[0m'
-BOLD_TEXT=$'\033[1m'
-UNDERLINE_TEXT=$'\033[4m'
-
 ## Get project id, project number, region, zone
 export PROJECT_ID=$(gcloud config get-value project)
 export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID \
@@ -35,17 +20,38 @@ echo
 #######################################################
 ## Task 1. Create an API key
 #######################################################
-#######################################################
-## Task 2. Make an entity analysis request
-#######################################################
 
 export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value core/project)
+
 gcloud iam service-accounts create my-natlang-sa \
   --display-name "my natural language service account"
-
+  
 gcloud iam service-accounts keys create ~/key.json \
   --iam-account my-natlang-sa@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
 
 export GOOGLE_APPLICATION_CREDENTIALS="/home/USER/key.json"
 
-gcloud compute ssh --zone "$ZONE" "linux-instance" --project "$DEVSHELL_PROJECT_ID" --quiet --command "gcloud ml language analyze-entities --content='Michelangelo Caravaggio, Italian painter, is known for \"The Calling of Saint Matthew\".' > result.json"
+#######################################################
+## Task 2. Make an entity analysis request
+#######################################################
+
+REMOTE_CMD=$(cat <<'EOF'
+gcloud ml language analyze-entities \
+  --content='Michelangelo Caravaggio, Italian painter, is known for "The Calling of Saint Matthew".' \
+  > result.json
+EOF
+)
+gcloud compute ssh \
+  --zone "$ZONE" "linux-instance" \
+  --project "$DEVSHELL_PROJECT_ID" \
+  --quiet \
+  --command "$REMOTE_CMD"
+
+echo "✅  ALL DONE"
+
+## Pretty print JSON
+gcloud compute ssh \
+  --zone "$ZONE" "linux-instance" \
+  --project "$DEVSHELL_PROJECT_ID" \
+  --quiet \
+  --command "jq . result.json"
