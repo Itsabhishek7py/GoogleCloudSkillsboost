cd7dfb6128c7d499171d3f7f95900a7e433f01f4
nov05 <1215590+nov05@users.noreply.github.com>
2026-05-12 15:57:51 -0500

    .

--- GSP1145 Create and Add Aspects to Knowledge Catalog Assets/nov05_gsp1145.sh
+++ GSP1145 Create and Add Aspects to Knowledge Catalog Assets/nov05_gsp1145.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## Created by nov05, 2026-05-11  
 
-cat >> ~/.bashrc <<'EOF'
+# cat >> ~/.bashrc <<'EOF'
 ## Get project id, project number, region, zone
 export PROJECT_ID=$(gcloud config get-value project)
 export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID \
@@ -19,8 +19,8 @@ echo "🔹  Region: $REGION"
 echo "🔹  Zone: $ZONE"
 echo "🔹  User: $USER"
 # echo "🔹  Bukect: $BUCKET"
-EOF
-source ~/.bashrc
+# EOF
+# source ~/.bashrc
 
 cat << 'EOF'
 
@@ -66,18 +66,15 @@ gcloud dataplex assets create customer-details-dataset \
 COMMENT
 
 ## Verify
-echo
-echo "👉  Data lake list:"
+echo -e "\n👉  Data lake list:"
 gcloud dataplex lakes list --location=$REGION
 
-echo
-echo "👉  Data zone list:"
+echo -e "\n👉  Data zone list:"
 gcloud dataplex zones list \
   --lake=orders-lake \
   --location=$REGION
 
-echo
-echo "👉  Data asset list:"
+echo -e "\n👉  Data asset list:"
 gcloud dataplex assets list \
   --lake=orders-lake \
   --zone=customer-curated-zone \
@@ -122,23 +119,30 @@ cat > aspect-type.json <<EOF
 }
 EOF
 
+## Create aspect type
 gcloud dataplex aspect-types create protected-data-aspect \
   --location=$REGION \
   --display-name="Protected Data Aspect" \
   --metadata-template-file-name=aspect-type.json
-
-echo "👉  Check entry list:"
+  
+## Verify
+echo -e "\n👉  Check entry list:"
 gcloud dataplex entries list \
   --location=$REGION \
   --entry-group=@dataplex
 export ASPECT_ENTRY_ID="protected-data-aspect_aspectType"
 
+echo -e "\n👉  Check entry $ASPECT_ENTRY_ID:"
+gcloud dataplex entries describe $ASPECT_ENTRY_ID \
+  --location=$REGION \
+  --entry-group=@dataplex
+
 ## Update aspect
 # gcloud dataplex catalog entries update $ASPECT_ENTRY_ID \
-#     --project=$PROJECT_ID \
-#     --location=$REGION \
-#     --aspect-keys="protected-data-aspect" \
-#     --aspect-data-file=aspect-type.json
+#   --project=$PROJECT_ID \
+#   --location=$REGION \
+#   --aspect-keys="protected-data-aspect" \
+#   --aspect-data-file=aspect-type.json
     
 cat << 'EOF'
 
@@ -151,19 +155,21 @@ https://docs.cloud.google.com/sdk/gcloud/reference/dataplex/entries/update
 EOF
 cat > aspect-patch.json <<EOF
 {
-  "protected_data_flag": "Yes" 
+  "resource": "projects/'"$PROJECT_NUMBER"'/locations/'"$REGION"'/aspectTypes/protected_data_aspect@customer_details": {
+    "data": {
+      "protected_data_flag": "Yes"
+    }
+  }
 }
 EOF
+
 # export TABLE_NAME="//bigquery.googleapis.com/projects/$PROJECT_ID/datasets/customers/tables/customer_details"
 # export TABLE_NAME="projects/$PROJECT_ID/locations/$REGION/entryGroups/@bigquery/entries/customer_details"
-export TABLE_NAME="projects/$PROJECT_ID/datasets/customers/tables/customer_details"
-gcloud dataplex entries update "$TABLE_NAME" \
-    --aspect-keys="protected-data-aspect" \
-    --aspect-data-file=aspect-patch.json
-gcloud dataplex entries update ASPECT_ENTRY_ID \
+# export TABLE_NAME="projects/$PROJECT_ID/datasets/customers/tables/customer_details"
+gcloud dataplex entries update $ASPECT_ENTRY_ID \
     --project=$PROJECT_ID \
     --location=$REGION \
-    --entry-group entry-group1 
+    --entry-group=@dataplex \ 
     --update-aspects=aspect-patch.json
   
 cat << 'EOF'
@@ -181,7 +187,16 @@ curl -X POST \
     "query": "customer_details",
     "scope": {
       "aspectTypes": [
-        "projects/'"$PROJECT_ID"'/locations/'"$REGION"'/aspectTypes/protected_data_aspect"
+        "projects/'"$PROJECT_NUMBER"'/locations/'"$REGION"'/aspectTypes/protected_data_aspect"
       ]
     }
+  }'
+
+
+curl -X POST \
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "Content-Type: application/json" \
+  "https://dataplex.googleapis.com/v1/projects/$PROJECT_ID/locations/$REGION:searchEntries" \
+  -d '{
+    "query": "customer_details"
   }'
