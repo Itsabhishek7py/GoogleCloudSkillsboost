51f55715e3186860b431c29baa9d12854e208b7d
nov05 <1215590+nov05@users.noreply.github.com>
2026-05-13 22:37:19 -0500

    .

---
+++ GSP480 How to Use a Network Policy on Google Kubernetes Engine/dev_logs_gsp480.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+## Created by nov05, 2026-05-13
+
+# Bright Foreground Colors
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+## Text format
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+# cat >> ~/.bashrc <<'EOF'
+## Get project id, project number, region, zone
+export PROJECT_ID=$(gcloud config get-value project)
+export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID \
+  --format='value(projectNumber)')
+export REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+export ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+# export BUCKET="$PROJECT_ID-bucket"
+# gcloud config set project $(gcloud projects list --format='value(PROJECT_ID)' --filter='qwiklabs-gcp')
+gcloud config set project $PROJECT_ID  
+gcloud config set compute/region $REGION
+echo
+echo "🔹  Project ID: $PROJECT_ID"
+echo "🔹  Project number: $PROJECT_NUMBER"
+echo "🔹  Region: $REGION"
+echo "🔹  Zone: $ZONE"
+echo "🔹  User: $USER"
+# echo "🔹  Bukect: $BUCKET"
+echo
+# EOF
+# source ~/.bashrc
+
+## Task 1. Lab setup
+
+gcloud config set compute/region $REGION
+gcloud config set compute/zone $ZONE
+
+## This will enable the necessary Service APIs, and it will also generate a terraform/terraform.tfvars file with the following keys.
+make setup-project
+cat terraform/terraform.tfvars
+make tf-apply
+
+## Task 2. Validation
+
+gcloud compute ssh gke-demo-bastion
+
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+echo "export USE_GKE_GCLOUD_AUTH_PLUGIN=True" >> ~/.bashrc
+source ~/.bashrc
+
+## Task 3. Installing the hello server
+
+
