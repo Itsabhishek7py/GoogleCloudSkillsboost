4ade07008a058a0d71fe5afbb79efbd0a53f2dee
nov05 <1215590+nov05@users.noreply.github.com>
2026-05-13 21:40:32 -0500

    Create nov05_gsp069.sh

---
+++ GSP069 App Engine: Qwik Start - PHP/nov05_gsp069.sh
@@ -0,0 +1,7 @@
+## Created by nov05, 2026-0513
+git clone https://github.com/GoogleCloudPlatform/php-docs-samples.git
+cd php-docs-samples/appengine/standard/helloworld
+sed -i 's/^runtime: php.*/runtime: php83/' app.yaml
+grep runtime app.yaml
+gcloud app deploy --quiet
+gcloud app browse
